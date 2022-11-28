9e4f9de2fb0370a483247d3e6f3afb3dcb5dc660
divby0exc <seddigh.daniel@gmail.com>
2022-11-28 21:31:40 +0100

    finished client code

--- DesignPattern/BuilderPattern/src/Client.java
+++ DesignPattern/BuilderPattern/src/Client.java
@@ -3,13 +3,34 @@ import java.time.LocalDate;
 public class Client {
 
     public static void main(String[] args) {
+        User user = createUser();
+        UserDTOBuilder builder = new UserWebDTOBuilder();
+        //Client has to provide director with concrete builder
+        UserDTO dto = directBuild(builder, user);
+        System.out.println(dto);
+    }
+    // Method to serve the role of director in builder pattern.
 
+    private static UserDTO directBuild(UserDTOBuilder builder, User user) {
+        return builder.withFirstName(user.getFirstName()).withLastName(user.getLastName())
+                .withAddress(user.getAddress())
+                .withBirthday(user.getBirthday())
+                .build();
     }
 
 
     public static User createUser() {
         User user = new User();
         user.setBirthday(LocalDate.of(1987,1,5));
-        user.setFirstName();
+        user.setFirstName("Bulan");
+        user.setLastName("Finski");
+        Address address = new Address();
+        address.setHouseNumber("66");
+        address.setStreet("Bulansväg");
+        address.setCity("Helsinki");
+        address.setState("Midgard");
+        address.setZipcode("11111");
+        user.setAddress(address);
+        return user;
     }
 }
