838431eb075660ff3e2468ca1ab6ff5ff11fe9b8
divby0exc <seddigh.daniel@gmail.com>
2023-01-27 16:38:37 +0100

    initiating sql didn't work of obvious reasons. bcuz I added receipts earlier on other users. which the newly added users had their id which made the program confused.

--- visma/src/main/java/com/divby0exc/visma/VismaApplication.java
+++ visma/src/main/java/com/divby0exc/visma/VismaApplication.java
@@ -17,18 +17,26 @@ public class VismaApplication {
 // TODO: 2023-01-25
 /**
  * Implement edit and delete method in repo
+ * done
  *
  * Implement methods from repo into edit and delete controller
- *
+ *dpme
  * Retrieve info from an invoice etc
- *
+ *done
  * Add rest of the repo methods in service and evaluate things
- *
+ *dome
  * Design the pages a bit better
- *
+ *done
  * implement the new login page
- *
+ * done
  * implement the new register page
- *
+ * dpme
  * find a nicer form to register receipt
+ *  done
  */
+// TODO
+/***
+ * Obvious flaws
+ * back button on edit page
+ */
+
